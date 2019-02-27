001e4a7222a01e9dadc12367ec76341a87dc9f46
potatowagon <th3bomberry@hotmail.com>
2019-02-27 18:39:48 +0800

    make validation a package to be imported from any directory

---
+++ validation/setup.py
@@ -0,0 +1,23 @@
+NAME = 'validation'
+VERSION = '1.0.0'
+SUMMARY = 'cg3002'
+AUTHOR = ''
+
+import os
+from distutils.core import setup
+
+old_cwd = os.getcwd()
+new_cwd = os.path.normpath(os.path.join(os.path.dirname(__file__), '..'))
+os.chdir(new_cwd)
+
+try:
+    setup(
+        name=NAME,
+        version=VERSION,
+        description=SUMMARY,
+        author=AUTHOR,
+        packages=[NAME],
+        package_data={NAME: ['*.so', '*.pyd', '*.dll', '*.dll', '*.properties', '*.ini', '*.info']}
+    )
+finally:
+    os.chdir(old_cwd)

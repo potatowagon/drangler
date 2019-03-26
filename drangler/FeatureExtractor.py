313fe6011eaa3635f299078bb0562a34ca8fc14d
Liwei <liliwei25@hotmail.com>
2019-03-26 14:05:22 +0800

    Added feature extractor for vertical data

--- drangler/FeatureExtractor.py
+++ drangler/FeatureExtractor.py
@@ -1,30 +1,46 @@
-import numpy
+import numpy as np
+from scipy.stats import iqr
 
 def extract(frame_collection):
     data_collection = []
     for frame in frame_collection:
         data = []
         for signal in frame:
-            # Adjust features here
-            data.append(mean(signal))
-            data.append(median(signal))
-            data.append(variance(signal))
+            data = np.append(data, get_features(signal))
 
-        data_collection.append(numpy.array(data))
-    return numpy.array(data_collection)
+        data_collection.append(np.array(data))
+    return np.array(data_collection)
 
+def get_features_from_frame(frame):
+    data = []
+    for i in range(0, frame.shape[1]):
+        data = np.append(data, get_features(frame[:, i]))
+    return data
+
+def get_features(signal):
+    return [
+        np.mean(signal),             # mean
+        np.var(signal),              # var
+        np.median(signal),           # median
+        iqr(signal),                 # iqr
+        np.std(signal),              # std
+        np.max(signal),              # max
+        np.min(signal),              # min
+        mad(signal)                  # mad
+    ]
 
 # Time domain
 def mean(signal):
-    return numpy.mean(signal)
+    return np.mean(signal)
 
 def median(signal):
-    return numpy.median(signal)
+    return np.median(signal)
 
 def variance(signal):
-    return numpy.var(signal)
+    return np.var(signal)
 
-
+def mad(data):
+    return np.mean(np.absolute(data - np.mean(data)))
 
 
 
